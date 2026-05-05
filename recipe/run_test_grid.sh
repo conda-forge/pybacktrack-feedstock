524efd37263a7e05d2ee0a64d4c82962d53b3ec4
John Cannon <john.cannon@sydney.edu.au>
2026-05-05 23:55:25 +1000

    Put grid test commands in a shell script to avoid quoting issues.

---
+++ recipe/run_test_grid.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -ex
+
+export PKG_PATH=$(python -c "import pybacktrack; import os; print(os.path.dirname(pybacktrack.__file__))")
+
+test -f "${PKG_PATH}/bundle_data/sediment_thickness/GlobSed.nc" || (echo "Grid file not found at ${PKG_PATH}/bundle_data/sediment_thickness/GlobSed.nc" && exit 1)
+
+ncdump -h "${PKG_PATH}/bundle_data/sediment_thickness/GlobSed.nc"
+
+gmt grdinfo "${PKG_PATH}/bundle_data/sediment_thickness/GlobSed.nc"
+
+echo "Sampling: normal"
+echo "150 -30" | gmt grdtrack -G"${PKG_PATH}/bundle_data/sediment_thickness/GlobSed.nc"
+
+echo "Sampling: zero index"
+echo "150 -30" | gmt grdtrack -G"${PKG_PATH}/bundle_data/sediment_thickness/GlobSed.nc?0"
+
+echo "Sampling: gdal"
+echo "150 -30" | gmt grdtrack -G"${PKG_PATH}/bundle_data/sediment_thickness/GlobSed.nc=gd"
